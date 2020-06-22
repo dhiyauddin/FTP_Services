507ffc6455b65ecdeda5d4f17cdfba86f484962a
dhiyauddin <dayau89@yahoo.com>
2020-06-22 13:14:23 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Logging;
+using Serilog;
+using System.IO;
+
+namespace FTPServices
+{
+    /*
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            ThreadService _obj = new ThreadService();
+            Thread _instance = new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        _obj.serviceCheckingFile();
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    Thread.Sleep(2880000);
+
+                }
+            });
+
+            _instance.Start();
+        }
+
+        
+    }
+    */
+
+    //------------------
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            ILogger configuration = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.RollingFile(@"D:\log\log.txt", retainedFileCountLimit: 7)
+                .CreateLogger();
+
+            StreamWriter writer =
+                new StreamWriter(File.Open(@"D:\seriself\seriself.txt", FileMode.Append, FileAccess.Write,
+                    FileShare.ReadWrite));
+
+            Serilog.Debugging.SelfLog.Out = writer;
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    ThreadService _obj = new ThreadService();
+                    _obj.serviceCheckingFile();
+                    writer.Flush();
+                    Thread.Sleep(60000);
+                }
+            });
+
+            return (int)HostFactory.Run(x =>
+            {
+                x.UseSerilog(configuration);
+                x.SetDescription("Service running for monitor,checking,upload and archieve file required.");
+                x.SetDisplayName("FTPUploadService");
+                x.SetServiceName("FTPUploadService");
+                x.Service<FTPUploadService>(sc =>
+                {
+                    sc.ConstructUsing(() => new FTPUploadService());
+                    sc.WhenStarted((s, hostControl) => s.Start(hostControl));
+                    sc.WhenStopped((s, hostControl) => s.Stop(hostControl));
+                });
+                x.StartAutomatically();
+            });
+        }
+    }
+
+    public class FTPUploadService : ServiceControl
+    {
+        private static readonly LogWriter Log = HostLogger.Get<FTPUploadService>();
+
+        public bool Start(HostControl hostControl)
+        {
+            Log.Info("FTPService is Starting...");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Log.Info("FTPService is Stopping...");
+            return true;
+        }
+    }
+}
---
+++ ThreadServices.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using FluentFTP;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.IO.Compression;
+using Topshelf.Logging;
+
+namespace FTPServices
+{
+    public class ThreadService
+    {
+        private static readonly LogWriter Log = HostLogger.Get<ThreadService>();
+        public void serviceCheckingFile()
+        {
+            string server = ConfigurationManager.AppSettings["server"];
+            string userName = ConfigurationManager.AppSettings["userName"];
+            string password = ConfigurationManager.AppSettings["password"];
+            string fileName = ConfigurationManager.AppSettings["fileName"];
+            string fullName = ConfigurationManager.AppSettings["fullName"];
+            string destinationName = ConfigurationManager.AppSettings["destinationName"];
+            string sourceName = ConfigurationManager.AppSettings["sourceName"];
+
+
+            FtpClient client = new FtpClient(server);
+            client.Credentials = new NetworkCredential(userName, password);
+            client.Connect();
+            //client.UploadFile(@fullName, destinationName + fileName);
+
+            Log.Info("--------------------------------");
+            Log.Info("FILE FROM SERVER:");
+            // get a list of files and directories in the "/ftp" folder
+            foreach (FtpListItem item in client.GetListing(destinationName))
+            {
+
+                // if this is a file
+                if (item.Type == FtpFileSystemObjectType.File)
+                {
+                    Log.Info(item);
+
+
+                    // get the file size
+                    long size = client.GetFileSize(item.FullName);
+
+                    // calculate a hash for the file on the server side (default algorithm)
+                    FtpHash hash = client.GetChecksum(item.FullName);
+
+                }
+
+                // get modified date/time of the file or folder
+                DateTime time = client.GetModifiedTime(item.FullName);
+            }
+
+            string[] filePaths = Directory.GetFiles(@sourceName);
+
+            Log.Info("--------------------------------");
+            Log.Info("FILE FROM LOCAL: (Matched with Server)");
+            int count = 1;
+            foreach (String item in filePaths)
+            {
+                string localFile = item.Replace(sourceName, "");
+
+                foreach (FtpListItem itemA in client.GetListing(destinationName))
+                {
+                    if (itemA.Type == FtpFileSystemObjectType.File)
+                    {
+                        if (localFile.Equals(itemA.Name))
+                        {
+                            Log.Info(count + ")" + itemA.Name);
+                            client.UploadFile(sourceName + localFile, destinationName + localFile);
+                            count++;
+                        }
+                    }
+                }                
+            }
+            compressfile(sourceName);
+            client.Disconnect();
+        }
+
+        void compressfile(string sourceName)
+        {
+            string today = DateTime.Now.ToString("yyyy_MM_dd");
+            string startPath = @sourceName;
+            string zipPath = @"C://Users//mdhiy//Downloads//archive_" + today + ".zip";
+            try
+            {
+                ZipFile.CreateFromDirectory(startPath, zipPath);
+            }
+            catch (Exception e) 
+            {
+                if(e!=null)
+                    Log.Info(e.InnerException);
+            }
+
+            decompressfile(today);
+
+        }
+        void decompressfile(String today)
+        {
+            string zipPath = @"C://Users//mdhiy//Downloads//archive_" + today + ".zip";
+            string extractPath = @"C://Users//mdhiy//Downloads//extract";
+            try
+            {
+                ZipFile.ExtractToDirectory(zipPath, extractPath);
+            }
+            catch (Exception e) 
+            {
+                if (e != null)
+                    Log.Info(e.InnerException);
+            }
+}
+    }
+}
